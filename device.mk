82bb14fc2ef925362ff59b39e79e1ea2e00477ee
Toucan-Sam <raharazod@gmail.com>
2019-01-15 16:02:44 -0500

    Add system props for IMS features
    * This fixes IMS for various carriers by allowing /system to access
      these prop calls.
    
    Change-Id: I37fba60584bf1e0251672acddb0c2d22119597aa

--- device.mk
+++ device.mk
@@ -158,3 +158,14 @@ PRODUCT_COPY_FILES += \
 # ModemService
 PRODUCT_COPY_FILES += \
     device/google/taimen/whitelist_modemservice.xml:system/etc/sysconfig/whitelist_modemservice.xml
+
+# system props for ims features
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
